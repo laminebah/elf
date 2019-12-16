731c4f02fa536629e6468197978589e4df950ddc
hafid <45362111+HafidHM@users.noreply.github.com>
2019-12-16 22:48:56 +0100

    Update print_content_section.h

--- print_content_section.h
+++ print_content_section.h
@@ -5,6 +5,8 @@
 
 #include <elf.h>
 #include <stdio.h>
+#include <ctype.h>
+
 
 #define N 16
 #define BUFF 8196
@@ -15,32 +17,10 @@
 #define JUMP24(S, A, T, P)	((S + A)| T) - P
 
 
-
-// typedef struct{
-// 	Elf32_Ehdr header;
-// 	Elf32_Shdr *section;
-// 	unsigned char *sectionHexa;
-// 	Elf32_Sym  *symtab;
-// 	unsigned char *symtable;
-// 	Elf32_Rel  *reltab;	
-// }Elf32_info;
-
-
-int get_section_num(Elf32_Shdr* sectionHeader, Elf32_Ehdr* header, char * name);
-
-//void affiche_contentSection(Elf32_Shdr sectionHexa, Elf32_Shdr* section, Elf32_Ehdr* header, FILE *file, char* name);
-
-void myhexdump(FILE*,int,int);
-
-//void content_section(char* name, Elf32_Shdr* section ,Elf32_Ehdr* entete, FILE *fichier);
-// void setup_little_endian(Elf32_info *elf);
-
-// void initElf(Elf32_info *elf,FILE *file);
-
-// void lire_Section_table(Elf32_info *,FILE *);
-
-
-
+int existe_section(unsigned char * sectionHexa, Elf32_Shdr* sectionHeader, Elf32_Ehdr* header, char * name);
+void affiche_contentSection(unsigned char * sectionHexa, Elf32_Shdr* section, Elf32_Ehdr* header, FILE *fichier, char* name);
+void hexdump(FILE*,int,int);
+void print_content_section(char* name, Elf32_Shdr* section ,Elf32_Ehdr* entete, FILE *fichier);
 
 
 #endif
