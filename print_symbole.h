8afa58f995b231522da36c5a65a5d67e8cd19908
labbobah <37943102+labbobah@users.noreply.github.com>
2019-12-17 13:17:29 +0100

    Update print_symbole.h

--- print_symbole.h
+++ print_symbole.h
@@ -1,1 +1,15 @@
+#ifndef __PRINT_SYMBOLE__
+#define __PRINT_SYMBOLE__
+
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_type(Elf32_Sym *sym);
+
+void print_link(Elf32_Sym *sym);
+
+void print_visi(Elf32_Sym *sym);
+
+#endif
 
