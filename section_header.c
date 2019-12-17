0e80321f8ad8a0b0ba87e28c38c94325debe81b7
LittlePinguin <58103824+LittlePinguin@users.noreply.github.com>
2019-12-17 16:28:01 +0100

    Add files via upload

--- section_header.c
+++ section_header.c
@@ -88,6 +88,9 @@ char *read_type (Elf32_Shdr s){
 		case SHT_HIUSER:
 			type = "HIUSER";
 			break;
+		case SHT_ARM_ATTRIBUTES:
+			type = "ARM_ATTRIBUTES";
+			break;
 		default:
 			break;
 	}
@@ -130,13 +133,13 @@ void print_section (FILE *file, Elf32_Shdr *T, Elf32_Ehdr *e){
 
 	printf("Il y a %d en-têtes de section, débutant à l'adresse de décalage %d :\n",e->e_shnum, e->e_entry);
 	printf("En-têtes de section : \n");
-	printf("[Nr]\t%-15s\t\t%-15s\t%-15s\t%-15s\t%s\t %2s\t %s\t\t%s\t%s\t%s\n","Nom","Type","Adr","Decala.","Taille","ES","Fan","LN","Inf","Al");	
+	printf("  [Nr] %-17s%-16s %-9s%-7s%-7s%-3s%-3s %-3s%-4s%-3s\n","Nom","Type","Adr","Decala.","Taille","ES","Fan","LN","Inf","Al");	
 	for (i=0; i<e->e_shnum; i++){	 //tant qu'on a des entrées à lire
 		S = T[i];
 
 		// Récupération du type 
 		type = read_type(S);
-		printf("[%d]\t", i);
+		printf("  [%2d]", i);
 		
 		//Récupération du flag
 		flag = malloc(sizeof(char*));
@@ -147,32 +150,54 @@ void print_section (FILE *file, Elf32_Shdr *T, Elf32_Ehdr *e){
 		fgets(name, SIZENAME, file);
 
 		// Affichage de tous les éléments de la section
-		printf("%-16s\t", name);
-		printf("%15s\t", type);
-		printf("%08x\t", S.sh_addr);
-		printf("%06x\t", S.sh_offset);
-		printf("%06x\t", S.sh_size);
-		printf("%02x\t", S.sh_entsize);
-		printf("%-15s\t",flag);
+		printf("%-17.17s\t", name);
+		printf("%-15s\t", type);
+		printf(" %08x", S.sh_addr);
+		printf(" %06x", S.sh_offset);
+		printf(" %06x", S.sh_size);
+		printf(" %02x", S.sh_entsize);
+		printf("  %2s",flag);
 		free(flag);
-		printf("%02d\t", S.sh_link);
-		printf("%03d\t", S.sh_info);
-		printf("%02d\t", S.sh_addralign);
+		printf(" %2d", S.sh_link);
+		printf("  %2d", S.sh_info);
+		printf(" %2d", S.sh_addralign);
 		printf("\n");
 	}
 	printf("\nClé des fanions : \n");
-	printf("W (écriture), A (allocation), X (exécution), M (fusion), S (chaînes)\n");
-	printf("I (info), L (ordre des liens), G (groupe), T (TLS), E (exclu), x (inconnu)\n");
-	printf("O (traitement additionnel requis pour l'OS), o (spécifique à l'OS), p (spécifique au processeur)\n");
+	printf("  W (écriture), A (allocation), X (exécution), M (fusion), S (chaînes)\n");
+	printf("  I (info), L (ordre des liens), G (groupe), T (TLS), E (exclu), x (inconnu)\n");
+	printf("  O (traitement additionnel requis pour l'OS), o (spécifique à l'OS), p (spécifique au processeur)\n");
 	free(name);
 }
 
-void get_sh_values(Elf32_Shdr **sectionHeader, FILE* f,Elf32_Ehdr *h){
+void elf_print_header(Elf32_Shdr **sectionHeader, FILE* f,Elf32_Ehdr *h){
 	fseek(f,h->e_shoff,SEEK_SET);
 	int i;
     for(i=0;i<h->e_shnum;i++)
     	read_section(f,&((*sectionHeader)[i]),*h);
 }
+
+int main(int argc, char *argv[]){
+
+
+	FILE *f;
+	Elf32_Ehdr *h;
+	Elf32_Shdr *sectionHeader;
+	f=fopen(argv[1],"r");
+    	h=lecture_entete(f);
+	sectionHeader=malloc(h->e_shnum*sizeof(Elf32_Shdr));
+    	elf_print_header(&sectionHeader,f,h);
+	print_section(f,sectionHeader,h);
+	free(sectionHeader);
+	fclose(f);
+	
+	
+	return 0;
+}
+
+
+
+
 
 
 
