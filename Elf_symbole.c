37f38407bfad4576d06a998809d8dc5843f6fd2f
laminebah <45362215+laminebah@users.noreply.github.com>
2019-12-17 20:22:25 +0100

    Update Elf_symbole.c

--- Elf_symbole.c
+++ Elf_symbole.c
@@ -1,10 +1,10 @@
-
 #include "Elf_symbole.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "section_header.h"
 #include <byteswap.h>
+#include "print_symbole.h"
 
 //lecture et affichage de la table des symboles
 Elf32_Sym *lecture_symb(FILE *felf,Elf32_Shdr *tab_section,Elf32_Ehdr elf_head,int *nbsymb,int *index) {
@@ -18,7 +18,8 @@ Elf32_Sym *lecture_symb(FILE *felf,Elf32_Shdr *tab_section,Elf32_Ehdr elf_head,i
 	assert (*nbsymb != -1);
 	symbtab = malloc (sizeof(Elf32_Sym)*(*nbsymb));
 	assert(symbtab != NULL);
-	for (int i = 0; i < *nbsymb ; i ++) {
+	int i = 0;
+	for (; i < *nbsymb ; i ++) {
 		
 		fseek (felf,tab_section[*index].sh_offset + i * tab_section [*index].sh_entsize,SEEK_SET);
 		assert(fread (&symbtab[i].st_name,sizeof(symbtab[i].st_name),1,felf) == 1);
@@ -38,3 +39,36 @@ Elf32_Sym *lecture_symb(FILE *felf,Elf32_Shdr *tab_section,Elf32_Ehdr elf_head,i
 	return symbtab;
 }
 
+void affiche_symbole_table (Elf32_Sym *symbtab,int nbsymb,int index,Elf32_Shdr *tab_section,FILE *felf) {
+	
+	//
+	char *tampon ;
+	int linksecsym = tab_section [index].sh_link;
+	assert ((tampon=malloc(tab_section[linksecsym].sh_size)) != NULL);
+	assert (fseek(felf,tab_section[linksecsym].sh_offset,SEEK_SET) != -1);
+	assert(fread (tampon,tab_section[linksecsym].sh_size,1,felf) == 1);
+	//assert (fread (tampon,tab_section[linksecsym].sh_size,1,felf) == tab_section[linksecsym].sh_size);
+	
+	printf("\n");
+	printf ("Table de symboles « .symtab » contient %d entrées:\n",nbsymb);
+	printf ("   Num:    Valeur Tail Type    Lien   Vis      Ndx Nom\n");
+	int i = 0;
+	for (; i < nbsymb; i ++) {
+		printf("   %3d: ", i);
+		printf("%08x  ", symbtab[i].st_value);
+		printf("%4d ", symbtab[i].st_size);
+		print_type_symb(symbtab[i].st_info);
+		print_link(symbtab[i].st_info);
+		print_visi(symbtab[i].st_other);
+		switch (symbtab[i].st_shndx) {
+			case SHN_UNDEF: printf ("UND ");
+				break;
+			case SHN_ABS:   printf ("ABS ");
+				break;
+			default :
+				printf("%3d ", symbtab[i].st_shndx);
+		}
+		printf("%s", tampon+symbtab[i].st_name);
+		printf("\n");
+	}
+}
