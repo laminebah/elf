87ffb9ccf26eebae84f6e59edbcad2f2b1708716
laminebah <45362215+laminebah@users.noreply.github.com>
2019-12-19 10:28:47 +0100

    Delete Elf_symbole.h

--- Elf_symbole.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H_SYMB
-#define _H_SYMB
-#include <stdio.h>
-#include <stdlib.h>
-#include <elf.h>
-
-Elf32_Sym *lecture_symb(FILE *felf,Elf32_Shdr *tab_section,Elf32_Ehdr elf_head,int *nbsymb,int *index);
-void affiche_symbole_table (Elf32_Sym *symbtab,int nbsymb,int index,Elf32_Shdr *tab_section,FILE *felf);
-#endif
