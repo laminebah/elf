a07264c360a9241e7233f2d30b6a5c1ad5c84a54
laminebah <45362215+laminebah@users.noreply.github.com>
2019-12-19 10:28:28 +0100

    Delete Elf_header.h

--- Elf_header.h
+++
@@ -1,8 +0,0 @@
-#ifndef _H_ELF_HEADER_
-#define _H_ELF_HEADER_
-#include <elf.h>
-#include <stdio.h>
-#include "util.h"
-int ElfId (Elf32_Ehdr * elf_head);
-Elf32_Ehdr * lecture_entete (FILE *felf);
-#endif 
