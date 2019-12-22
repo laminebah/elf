a841095d8822d1a5ee78b0c2ee561f34fdf81694
hafid <45362111+HafidHM@users.noreply.github.com>
2019-12-22 23:56:54 +0000

    fichier commantée

--- Myreadelf/print_content_section.h
+++ Myreadelf/print_content_section.h
@@ -1,26 +1,41 @@
 #ifndef __PRINT_CONTENT_SECTION_H__
 #define __PRINT_CONTENT_SECTION_H__
-
-
-
 #include <elf.h>
 #include <stdio.h>
 #include <ctype.h>
-
-
 #define N 16
-#define BUFF 8196
-#define ABS32(S, A, T)	(S + A)| T
-#define ABS16(S, A)	(S + A)
-#define ABS8(S, A)	(S + A)
-#define CALL(S, A, T, P)	((S + A)| T) - P
-#define JUMP24(S, A, T, P)	((S + A)| T) - P
+
+						            	/*  gère l'affichage du contenu d'une section   */
 
 
-int existe_section(unsigned char * sectionHexa, Elf32_Shdr* sectionHeader, Elf32_Ehdr* header, char * name);
-void affiche_contentSection(unsigned char * sectionHexa, Elf32_Shdr* section, Elf32_Ehdr* header, FILE *fichier, char* name);
+/***	Cette fonction test l' existance d' une section 
+		Données : une chaine de caractere, table des sections, l'entete et le nom d'une section
+		Résultat: entier(num de la section 'name' )
+***/
+int existe_section(unsigned char *, Elf32_Shdr*, Elf32_Ehdr*, char *);
+
+
+/***	Cette fonction affiche le contenu brute d'une section
+		Données : une chaine de caractere , table des sections, l'entete ,un fichier ELF et le nom d'une section
+		Résultat: aucun
+***/
+void affiche_contentSection(unsigned char *, Elf32_Shdr*, Elf32_Ehdr*, FILE *, char*);
+
+
+
+/***	Cette fonction est utilsée par la fonction affiche_contentSection  pour faire l`affichage en format hexa
+		Données :  un fichier ELF, une adresse et une taille
+		Résultat: aucun
+***/
 void hexdump(FILE*,int,int);
-unsigned char *print_content_section(char* name, Elf32_Shdr* section ,Elf32_Ehdr* entete, FILE *fichier, int bool);
+
+
+
+/***	Cette fonction permet de lire le contenu d`une section 
+		Données : nom d'une section, table des sections, l'entete , un fichier ELF et un boolean
+		Résultat: aucun
+***/
+unsigned char *print_content_section(char*, Elf32_Shdr*, Elf32_Ehdr*, FILE *, int);
 
 
 #endif
