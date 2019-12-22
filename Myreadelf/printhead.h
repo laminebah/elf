3ee25cdec2991753c3cf41c64f7ab69dc64e8d19
laminebah <45362215+laminebah@users.noreply.github.com>
2019-12-22 17:25:21 +0100

    Update printhead.h

--- Myreadelf/printhead.h
+++ Myreadelf/printhead.h
@@ -1,14 +1,7 @@
 #ifndef _H_PHEAD_H
 #define _H_PHEAD_H
 
-/****gère l'affichage de l'entete du fichier*****/
-/**structure pour géner les différents strings à afficher***/
-
-typedef struct {
-    int i;
-    const char *chaine;
-}stringTypes;
-
+/*Ce module genre l'affichage de l'entete du fichier elf */
 void print_e_ident(Elf32_Ehdr *entete);
 void print_type(Elf32_Ehdr *entete);
 void print_classe_type(Elf32_Ehdr *entete);
@@ -17,6 +10,11 @@ void print_v_machine(Elf32_Ehdr *entete);
 void print_OS(Elf32_Ehdr *entete);
 void print_e_version(Elf32_Ehdr *entete);
 void print_version (Elf32_Ehdr *entete);
-void print_header(Elf32_Ehdr *entete);
 void print_flags (Elf32_Ehdr *entete);
+/***Cette fonction fait appel à toutes ses fonctions précendente
+	Données : une structure d'entete elf
+	Résultat: Aucun
+***/
+void print_header(Elf32_Ehdr *entete);
+
 #endif
