1ce2afc71a9aae25582b834f6786781af78a38d4
laminebah <45362215+laminebah@users.noreply.github.com>
2019-12-22 16:34:50 +0100

    Update Elf_header.h

--- Myreadelf/Elf_header.h
+++ Myreadelf/Elf_header.h
@@ -3,12 +3,20 @@
 #include <elf.h>
 #include <stdio.h>
 #include "util.h"
-int ElfId (Elf32_Ehdr * elf_head);
+
+/***-initialise la table d'entete elf et fait appel à la fonction ElfId;
+    -Donnees: Elle prend un fichier ouvert en lecture;
+    -Resultat: 
+    		 - NULL si la structure n'a pas été initialisée (fichier non elf ou erreur allocation)
+    		 - une structure (Elf32_Ehdr *) initialisée ;
+***/
 Elf32_Ehdr * lecture_entete (FILE *felf);
-
-
-//convert little endian
-int32_t value_32(int32_t value);
-int16_t value_16(int16_t value);
-
+/***-test du format du fichier
+	-Donées: Une structure (entete elf) initialisée;
+	-Résultat: 
+			- -1 si la structure n'est pas au format elf c'est à dire que les nombres magiques ne 
+			  correspondent pas au format attendu ou que la classe n'est pas de ELF32;
+			- 0 si le format est celui attendu;
+***/
+int ElfId (Elf32_Ehdr * elf_head);
 #endif 
