4dbfb8e0a00bea9b42bfe521d201b035a6d0d640
hafid <45362111+HafidHM@users.noreply.github.com>
2020-01-08 00:02:34 +0000

    ecriture de la table des sections , manque lecriture du nom d'une section

--- Myreadelf/fusion.c
+++ Myreadelf/fusion.c
@@ -61,7 +61,7 @@ void fusion_section1_in_section2(Donnees* d,FILE * file_in1, FILE* file_in2, Elf
 	int idx;
 	for (int i = 1; i < d->nbS1; ++i){
 		if(d->sh1[i].sh_type == type){
-			if(((idx=is_in(get_section_name(h1, d->sh1, i, file_in1), h2 , d->sh2, file_in2)) != -1) && d->f[i].newsh == NULL){
+			if((idx=is_in(get_section_name(h1, d->sh1, i, file_in1), h2 , d->sh2, file_in2)) != -1){
 					 d->f[i].newsh = malloc(sizeof(Elf32_Shdr)*2);
 					 assert(d->f[i].newsh != NULL);
 
@@ -74,11 +74,16 @@ void fusion_section1_in_section2(Donnees* d,FILE * file_in1, FILE* file_in2, Elf
 						 d->f[i].name = get_section_name(h1, d->sh1, i, file_in1);
 					
 						/*modificaion des offsets des deux sections a fusionnees*/
-						 d->offset = d->f[i].newsh[0].sh_offset;
-						 d->f[i].newsh[1].sh_offset = d->f[i].newsh[0].sh_offset + d->f[i].newsh[0].sh_size;
+						 if (i==1){
+						 	d->offset = d->f[i].newsh[0].sh_offset;
+						 }
+						 d->f[i].offset = d->offset;
+						 d->f[i].newsh[0].sh_offset = d->offset;
+						
 
 						/* modificaion du size global des deux sections a fusionnees */
-						d->f[i].size = d->f[i].newsh[0].sh_size +  d->f[i].newsh[1].sh_size;
+						d->f[i].size = d->sh1[i].sh_size +  d->sh2[idx].sh_size;	
+						d->f[i].newsh[0].sh_size += d->sh2[idx].sh_size;
 
 			}else{
 				if (d->f[i].newsh == NULL){
@@ -92,20 +97,24 @@ void fusion_section1_in_section2(Donnees* d,FILE * file_in1, FILE* file_in2, Elf
 					d->f[i].name = get_section_name(h1, d->sh1, i, file_in1);
 
 					/* modificaion du size global */
-					d->f[i].size = d->sh1[i].sh_size;
+					d->f[i].size = d->sh1[0].sh_size;
 
 					/*modificaion d'offset d'une section n'existe pas dans le deuxieme fichier  */
 					d->f[i].newsh[0].sh_offset = d->offset;
+					d->f[i].offset = d->offset;
+
 					
 				}
 			}
 
-			/* modification des offsets globals  */
-			d->f[i].offset = d->f[i].newsh[0].sh_offset;
-			d->offset = d->f[i].offset + d->f[i].size; 
+			/* modification des offsets <de la section i> et de l'offset global  */
+					d->f[i].newsh[0].sh_offset = d->offset;
+					d->offset += d->f[i].size;
 		}
 	}
 }
+
+
 void fusion_section2_in_section1(Donnees* d,FILE * file_in1, FILE* file_in2, Elf32_Ehdr* h1 ,Elf32_Ehdr* h2, Elf32_Word type){
 	for (int i = 1; i < d->nbS2; ++i){
 		if(d->sh2[i].sh_type == type){
@@ -114,24 +123,24 @@ void fusion_section2_in_section1(Donnees* d,FILE * file_in1, FILE* file_in2, Elf
 					d->f = (Donnees_fusion *)realloc(d->f,sizeof(Donnees_fusion) * d->nbS1);
 					d->f[d->nbS1-1].newsh = malloc(sizeof(Elf32_Shdr)*1);
 				 	assert(d->f[d->nbS1-1].newsh != NULL);
+
 					memcpy(&d->f[d->nbS1-1].newsh[0],&d->sh2[i], sizeof(Elf32_Shdr));
 					d->f[d->nbS1-1].type = type;
 					d->f[d->nbS1-1].nbS = 1;
-
 					/* copie le nom de la section */
 					d->f[d->nbS1-1].name = get_section_name(h2, d->sh2, i, file_in2);
 
-					/* modificaion du size global */
-					d->f[d->nbS1-1].size =  d->sh2[i].sh_size;
-
-					/*modificaion d'offset d'une section n'existe pas dans le premier fichier  */
-					d->f[d->nbS1-1].newsh[0].sh_offset = d->offset;
+					/*  Modification du size et les offsets */
+				 	d->f[d->nbS1-1].size = d->sh2[i].sh_size;
 					d->f[d->nbS1-1].offset = d->offset;
-					d->offset += d->f[d->nbS1-1].size; 
+					d->f[d->nbS1-1].newsh[0].sh_offset = d->offset;
+					d->offset += d->f[d->nbS1-1].size;
 				}
 		}
 	}
 }
+
+
 void fusion_by_type(Donnees* d,FILE * file_in1, FILE* file_in2, Elf32_Ehdr* h1 ,Elf32_Ehdr* h2, Elf32_Word type){
 	if(type == SHT_NULL){
 		d->f[0].newsh = malloc(sizeof(Elf32_Shdr)*1);
@@ -219,13 +228,13 @@ void modification_indx_sections(Donnees * d){
 }
 
 
+
 void ecriture_entete(Elf32_Ehdr* elf_head, FILE* file, Donnees* d){
 
 	elf_head->e_shoff = d->offset;
 	elf_head->e_shnum = d->nbS1;
 	size_t nmemb;
 
-	//debug("Il y a %u sections dans le nouveau fichier ELF créé.\n", elf_head->e_shnum);
 	fseek(file, 0, SEEK_SET);
 	nmemb = fwrite(elf_head->e_ident,   EI_NIDENT,   1, file); assert(nmemb == 1);
 	nmemb = fwrite(&elf_head->e_type, sizeof(elf_head->e_type), 1, file); assert(nmemb == 1);
@@ -243,7 +252,6 @@ void ecriture_entete(Elf32_Ehdr* elf_head, FILE* file, Donnees* d){
 	nmemb = fwrite(&elf_head->e_shstrndx,  sizeof(elf_head->e_shstrndx), 1, file); assert(nmemb == 1);
 
 }
-
 
 void ecriture_section_table(Elf32_Ehdr* elf_head, FILE* file, Donnees* d){
 	Elf32_Off offset = 0;
@@ -268,21 +276,32 @@ void ecriture_section_table(Elf32_Ehdr* elf_head, FILE* file, Donnees* d){
 
 	//debug("Il y a %d en-têtes de section, débutant à l'adresse de décalage 0x%x dans le nouveau fichier créé:\n\n",elf_head->e_shnum, elf_head->e_shoff);
 	fseek(file, elf_head->e_shoff, SEEK_SET);
-	for(int i = 0; i < d->nbS1; i++)
-	{
-		//debug("Écriture de l'en-tête de section n°%2i  dans le fichier à l'offset %#x ", i, d->offset + i * elf_head->e_shentsize);
+	for(int i = 0; i < d->nbS1; i++){
+		
 		fseek(file, d->offset + i * elf_head->e_shentsize, SEEK_SET);
+		offset += fwrite(&d->f[i].newsh[0].sh_name,   sizeof(d->f[i].newsh[0].sh_name), 1, file);
+		offset += fwrite(&d->f[i].type,      sizeof(d->f[i].type), 1, file);
+		offset += fwrite(&d->f[i].newsh[0].sh_flags,     sizeof(d->f[i].newsh[0].sh_flags), 1, file);
+		offset += fwrite(&d->f[i].newsh[0].sh_addr,      sizeof(d->f[i].newsh[0].sh_addr), 1, file);
+		offset += fwrite(&d->f[i].newsh[0].sh_offset,    sizeof(d->f[i].offset), 1, file);
+		offset += fwrite(&d->f[i].newsh[0].sh_size,      sizeof(d->f[i].size), 1, file);
+		offset += fwrite(&d->f[i].sh_link,      sizeof(d->f[i].sh_link), 1, file);
+		offset += fwrite(&d->f[i].sh_info,      sizeof(d->f[i].sh_info), 1, file);
+		offset += fwrite(&d->f[i].newsh[0].sh_addralign, sizeof(d->f[i].newsh[0].sh_addralign), 1, file);
+		offset += fwrite(&d->f[i].newsh[0].sh_entsize,   sizeof(d->f[i].newsh[0].sh_entsize), 1, file);
+		if(d->f[i].nbS==2){
+					offset += fwrite(&d->f[i].newsh[1].sh_name,   sizeof(d->f[i].newsh[1].sh_name), 1, file);
+					offset += fwrite(&d->f[i].type,  sizeof(d->f[i].type), 1, file);
+					offset += fwrite(&d->f[i].newsh[1].sh_flags,     sizeof(d->f[i].newsh[1].sh_flags), 1, file);
+					offset += fwrite(&d->f[i].newsh[1].sh_addr,      sizeof(d->f[i].newsh[1].sh_addr), 1, file);
+					offset += fwrite(&d->f[i].newsh[1].sh_offset,    sizeof(d->f[i].offset), 1, file);
+					offset += fwrite(&d->f[i].newsh[1].sh_size,      sizeof(d->f[i].size), 1, file);
+					offset += fwrite(&d->f[i].sh_link,      sizeof(d->f[i].sh_link), 1, file);
+					offset += fwrite(&d->f[i].sh_info,      sizeof(d->f[i].sh_info), 1, file);
+					offset += fwrite(&d->f[i].newsh[1].sh_addralign, sizeof(d->f[i].newsh[1].sh_addralign), 1, file);
+					offset += fwrite(&d->f[i].newsh[1].sh_entsize,   sizeof(d->f[i].newsh[1].sh_entsize), 1, file);
+		}
 
-		offset += fwrite(&d->sh1[i].sh_name,      sizeof(d->sh1[i].sh_name), 1, file);
-		offset += fwrite(&d->sh1[i].sh_type,      sizeof(d->sh1[i].sh_type), 1, file);
-		offset += fwrite(&d->sh1[i].sh_flags,     sizeof(d->sh1[i].sh_flags), 1, file);
-		offset += fwrite(&d->sh1[i].sh_addr,      sizeof(d->sh1[i].sh_addr), 1, file);
-		offset += fwrite(&d->sh1[i].sh_offset,    sizeof(d->sh1[i].sh_offset), 1, file);
-		offset += fwrite(&d->sh1[i].sh_size,      sizeof(d->sh1[i].sh_size), 1, file);
-		offset += fwrite(&d->sh1[i].sh_link,      sizeof(d->sh1[i].sh_link), 1, file);
-		offset += fwrite(&d->sh1[i].sh_info,      sizeof(d->sh1[i].sh_info), 1, file);
-		offset += fwrite(&d->sh1[i].sh_addralign, sizeof(d->sh1[i].sh_addralign), 1, file);
-		offset += fwrite(&d->sh1[i].sh_entsize,   sizeof(d->sh1[i].sh_entsize), 1, file);
 	}
 	d->offset = offset;
 }
