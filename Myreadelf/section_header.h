c5fcfb7454c7fcb45233437e4d1205cb41a83802
samibk201 <45363113+samibk201@users.noreply.github.com>
2020-01-09 09:30:13 +0100

    Update section_header.h

--- Myreadelf/section_header.h
+++ Myreadelf/section_header.h
@@ -8,31 +8,70 @@
 #include <assert.h>
 #include "Elf_header.h"
 
-#define SIZENAME 30
-//-entrees : une entete h et un entier sur 32 bits value,
-//-sortie : l'entier value en little endian
+#define SIZENAME 100
+
+
+/**
+  * Retourne l'entier passé en paramètre en little endian
+  * Paramètres :  h une entête
+                  value un entier sur 32 bits
+**/
 int32_t val_32(Elf32_Ehdr h, int32_t value);
 
-//-entrees : une entete h et un entier sur 16 bits value,
-//-sortie : l'entier value en little endian
+
+
+/**
+  * Retourne l'entier passé en paramètre en little endian
+  * Paramètres :  h une entête 
+                  value un entier sur 16 bits
+**/
 int16_t val_16(Elf32_Ehdr h, int16_t value);
 
 
-//-entrees: un fichier, une section et une entete
-//-sortie: NULL
-//-la fonction affecte les bonne valeurs (en little endian) de la section du fichier f a la section s
+
+/**
+  * Affecte les bonne valeurs (en little endian) de la section du fichier f à la section s
+  * Paramètres :  fichier le fichier qu'on lit 
+                  S une section
+                  h une entête
+**/
 void read_section(FILE *fichier, Elf32_Shdr *s, Elf32_Ehdr h);
 
-//retourne la chaine de characteres correspondate au type de la section s
+
+
+/**
+  * Retourne le type de la section s sous forme de chaîne de caractères
+  * Paramètres : s une section
+**/
 char *read_type (Elf32_Shdr s);
 
-//retourne la chaine de characteres correspondate au flag de la section s
+
+
+/**
+  * Retourne le flag de la section s sous forme de chaîne de caractères
+  * Paramètres :  flag le  flag qu'on récupère pour lire
+                  s une section 
+**/
 char *read_flags (char *flag, Elf32_Word s);
 
-//pour afficher la section T d'un fichier file
+
+
+/**
+  * Affiche la section T d'un fichier file
+  * Paramètres :  file le fichier qu'on lit
+                  T une section
+                  e une structure de type Elf32_Ehdr
+**/
 void print_section (FILE *file, Elf32_Shdr *T, Elf32_Ehdr *e);
 
-//la fonction recupere toutes les valeurs de la section en little endian et les met dans sectionHeader
+
+
+/**
+  * Récupère toutes les valeurs de la section en little endian et les mets dans sectionHeader
+  * Paramètres :  sectionHeader la section dans laquelle on met les valeurs récupérées
+                  f le fichier qu'on lit
+                  h une structure de type Elf32_Ehdr
+**/
 void get_sh_values(Elf32_Shdr **sectionHeader, FILE* f,Elf32_Ehdr *h);
 
 #endif
