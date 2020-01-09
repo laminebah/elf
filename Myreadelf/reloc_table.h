4f254d1e3463c7b440915468195f88c7b5d672a1
samibk201 <45363113+samibk201@users.noreply.github.com>
2020-01-09 09:12:27 +0100

    Add files via upload

--- Myreadelf/reloc_table.h
+++ Myreadelf/reloc_table.h
@@ -10,22 +10,61 @@
 #include "section_header.h"
 
 
-//recupere le contenu de la table de realocation et le renvoie
+/**
+	* Récupère le contenu de la table des réalocations et le renvoie
+	* Paramètres : 	h une structure de type Elf32_Ehdr
+					S une structure de type Elf32_Shdr
+					file le fichier qu'on lit
+					index l'index de position
+	* Retourne une table de réalocations
+**/
 Elf32_Rel *read_REL_tab(Elf32_Ehdr h, Elf32_Shdr *S, FILE *file, int index);
 
-//un simple affiche de la premiere ligne du tableau de realocation
+
+
+/**
+	* Affiche la 1ère ligne de la table de réalocations
+	* Paramètres : 	RelSize la taille de la table
+					sh_name le nom de la section
+					sh_offset le déccalage de la section
+**/
 void print_RelTab_head(int RelSize, unsigned char *sh_name, unsigned int sh_offset);
 
-//retourne la chaine de caracteres qui correspond au type de la tble de realocation
+
+
+/**
+	* Retourne la chaîne de caractères qui correspond au type de la table de réalocations
+	* Paramètres :	REL_tab la table de réalocations qu'on lit
+**/
 char * reloc_types(Elf32_Rel REL_tab);
 
-//retourne le type de la table des symboles qui correspond comme chaine de caracteres
+
+
+/**
+	* Retourne le type du symbole sous forme de chaîne de caractères
+	* Paramètres :	type le type du symbole qu'on lit
+**/
 char* get_sym_type (unsigned int type);
 
-//un affichage du contenu de la table de realocation
+
+
+/**
+	* Affiche le contenu de la table de réalocations
+	* Paramètres : 	Sec une structure de type Elf32_Shdr
+					h une structure de type Elf32_Ehdr
+					Sym une structure de type Elf32_Sym
+					file le fichier qu'on lit
+**/
 void affiche_Relocation(Elf32_Shdr*Sec, Elf32_Ehdr h, Elf32_Sym *Sym, unsigned char *strtab, FILE *file);
 
-//renvoie le nom d'une table des symboles comme chaine de caracteres
+
+
+/**
+	* Renvoie le nom d'une table des symboles sous forme de chaîne de caractères
+	* Paramètres : 	Sec une structure de type Elf32_Shdr
+					index l'index de position
+					file le fichier qu'on lit
+**/
 char* SymName(Elf32_Shdr *Sec, int index, int indexStrTab, FILE *file);
 
 #endif
