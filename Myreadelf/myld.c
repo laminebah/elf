3c91c76e4655efc15c792bce965bb49a78cb7d5c
scartelo <58155534+scartelo@users.noreply.github.com>
2020-01-09 11:06:34 +0100

    Update myld.c

--- Myreadelf/myld.c
+++ Myreadelf/myld.c
@@ -41,11 +41,17 @@ int main (int argc, char** argv){
     elf_head1 = lecture_entete(file_in1);
     elf_head2 = lecture_entete(file_in2);
 
+    Elf32_Sym *symbTab1,*symbTab2;
+    int index1,index2,nbsymb1,nbsymb2;
+    SymbTab * sout;
 
     sections_table1=malloc(elf_head1->e_shnum*sizeof(Elf32_Shdr));
     get_sh_values(&sections_table1 , file_in1 , elf_head1);
     sections_table2=malloc(elf_head2->e_shnum*sizeof(Elf32_Shdr));
     get_sh_values(&sections_table2 , file_in2 , elf_head2);
+
+    symbTab1 = lecture_symb(file_in1,sections_table1,*elf_head1,&nbsymb1,&index1);
+    symbTab2 = lecture_symb(file_in2,sections_table2,*elf_head2,&nbsymb2,&index2);
 
     Donnees* d = malloc(sizeof(Donnees));
 
@@ -56,6 +62,10 @@ int main (int argc, char** argv){
   for (int i = 0; i < 8; ++i)
       fusion_by_type(d,file_in1,file_in2, elf_head1 ,elf_head2,s_types[i]);
 
+  /**fusion des symboles **/
+ if ((sout=fusion_symbols (d,sections_table1,sections_table2,elf_head1,elf_head2, symbTab1,symbTab2,file_in1,file_in2,nbsymb2,index2,index1,nbsymb1)) == NULL) 
+      exit (1);
+
   /* modification des attributs sh_link et sh_info */
   modification_indx_sections(d);
   /* ecriture de l'entete  */
@@ -63,11 +73,21 @@ int main (int argc, char** argv){
   /* ecriture de la table des de section et la table des noms des sections   */
   ecriture_section_table(elf_head1, file_out , d);
 
+  /*  Ecriture des symboles  */
+  ecris_new_symb_in_file (file_out,d,sout);
+  free (symbTab2);
+  free (symbTab1);
+
    /* liberation de la memoire utilsee  */
   liberer_elf( elf_head1 , elf_head2 , sections_table1, sections_table2);
   liberer_fusion(d);
 
+
+
    /* fermer les fichiers overts  */
+  free (sout->nomSymbs);
+  free (sout->nom);
+  free (sout);
   fermer_fichiers(file_in1,file_in2,file_out);
 
   return EXIT_SUCCESS;
